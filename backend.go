bf3859e598f521d368eb6485fc60a87ae6814557
Dan Stutzman <dan@danstutzman.com>
2016-09-05 22:35:35 -0600

    Moved Postgres connection from backend.go to model/postgres.go

--- backend.go
+++ backend.go
@@ -3,11 +3,9 @@ package main
 import (
 	"./model"
 	"./web"
-	"database/sql"
 	"encoding/json"
 	"flag"
 	"fmt"
-	_ "github.com/lib/pq"
 	"log"
 	"os"
 )
@@ -37,53 +35,19 @@ func mustParseFlags() CommandLineArgs {
 	return args
 }
 
-func mustOpenPostgres(postgresCredentialsPath string) *sql.DB {
-	postgresCredentialsFile, err := os.Open(postgresCredentialsPath)
+func readPostgresCredentials(path string) model.PostgresCredentials {
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(fmt.Errorf("Couldn't os.Open postgres_credentials: %s", err))
 	}
-	defer postgresCredentialsFile.Close()
+	defer file.Close()
 
-	type PostgresCredentials struct {
-		Username     *string
-		Password     *string
-		DatabaseName *string
-		SSLMode      *string
+	creds := model.PostgresCredentials{}
+	decoder := json.NewDecoder(file)
+	if err = decoder.Decode(&creds); err != nil {
+		log.Fatalf("Error using decoder.Decode to parse JSON at %s: %s", path, err)
 	}
-	postgresCredentials := PostgresCredentials{}
-	decoder := json.NewDecoder(postgresCredentialsFile)
-	if err = decoder.Decode(&postgresCredentials); err != nil {
-		log.Fatalf("Error using decoder.Decode to parse JSON at %s: %s",
-			postgresCredentialsPath, err)
-	}
-
-	dataSourceName := ""
-	if postgresCredentials.Username != nil {
-		dataSourceName += " user=" + *postgresCredentials.Username
-	}
-	if postgresCredentials.Password != nil {
-		dataSourceName += " password=" + *postgresCredentials.Password
-	}
-	if postgresCredentials.DatabaseName != nil {
-		dataSourceName += " dbname=" + *postgresCredentials.DatabaseName
-	}
-	if postgresCredentials.SSLMode != nil {
-		dataSourceName += " sslmode=" + *postgresCredentials.SSLMode
-	}
-
-	db, err := sql.Open("postgres", dataSourceName)
-	if err != nil {
-		log.Fatal(fmt.Errorf("Error from sql.Open: %s", err))
-	}
-
-	// Test out the database connection immediately to check the credentials
-	ignored := 0
-	err = db.QueryRow("SELECT 1").Scan(&ignored)
-	if err != nil {
-		log.Fatal(fmt.Errorf("Error from db.QueryRow: %s", err))
-	}
-
-	return db
+	return creds
 }
 
 func main() {
@@ -91,7 +55,8 @@ func main() {
 
 	var model_ model.Model
 	if args.postgresCredentialsPath != "" {
-		model_ = model.NewDbModel(mustOpenPostgres(args.postgresCredentialsPath))
+		creds := readPostgresCredentials(args.postgresCredentialsPath)
+		model_ = model.NewDbModel(model.MustOpenPostgres(creds))
 	} else if args.inMemoryDb {
 		model_ = &model.MemoryModel{
 			NextDeviceId: 1,
---
+++ model/postgres.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type PostgresCredentials struct {
+	Username     *string
+	Password     *string
+	DatabaseName *string
+	SSLMode      *string
+}
+
+func MustOpenPostgres(creds PostgresCredentials) *sql.DB {
+	dataSourceName := ""
+	if creds.Username != nil {
+		dataSourceName += " user=" + *creds.Username
+	}
+	if creds.Password != nil {
+		dataSourceName += " password=" + *creds.Password
+	}
+	if creds.DatabaseName != nil {
+		dataSourceName += " dbname=" + *creds.DatabaseName
+	}
+	if creds.SSLMode != nil {
+		dataSourceName += " sslmode=" + *creds.SSLMode
+	}
+
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Error from sql.Open: %s", err))
+	}
+
+	// Test out the database connection immediately to check the credentials
+	ignored := 0
+	err = db.QueryRow("SELECT 1").Scan(&ignored)
+	if err != nil {
+		log.Fatal(fmt.Errorf("Error from db.QueryRow: %s", err))
+	}
+
+	return db
+}
